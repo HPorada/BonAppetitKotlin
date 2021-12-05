ce00c968fb5e474fdba045f76e8414145d9f4fc1
HPorada <HPorada@users.noreply.github.com>
2021-12-05 23:08:57 +0100

    MealsRecyclerActivity to be done.

--- app/src/main/java/com/example/bonappetitkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bonappetitkotlin/MainActivity.kt
@@ -5,17 +5,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.squareup.picasso.Picasso
-import org.json.JSONException
+import org.json.JSONArray
 import org.json.JSONObject
 
 // DONE:
@@ -27,7 +27,7 @@ import org.json.JSONObject
 // Polymorphism 10 - MealUser, MealAPI (getMealData)
 // Data classes 3 - DataAPI
 // Exceptions 5 -> try/catch przy łączeniu z api
-// Lambdas 10 -> response (MainActivity), sortowanie?
+// Lambdas 10 -> response (MealsRecyclerActivity), sortowanie?
 
 
 // TODO
@@ -55,18 +55,16 @@ import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    val mealsAPI: ArrayList<Meal> = ArrayList<Meal>()
+    var mealsAPI: MutableList<MealAPI> = mutableListOf()
     var randomMeal: Meal = MealAPI()
-    var jObject: JSONObject = JSONObject()
-    val mealsUser: ArrayList<Meal> = ArrayList<Meal>()
+    lateinit var jObject: JSONObject
+    var mealsUser: ArrayList<Meal> = ArrayList<Meal>()
 
     val data = DataAPI()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        createMealList()
 
         val randomRecipeImage = findViewById<View>(R.id.imgRandomMeal) as ImageView
         val randomRecipeName = findViewById<View>(R.id.txtRandomName) as TextView
@@ -75,8 +73,7 @@ class MainActivity : AppCompatActivity() {
         val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
         val stringRequest: StringRequest =
-            StringRequest(Request.Method.GET, data.randomURL, Response.Listener { response ->
-
+            StringRequest(Request.Method.GET, data.randomURL, { response ->
                 val dataMeal: DataMeal = gson.fromJson(response, DataMeal::class.java)
                 randomMeal = dataMeal.getMealsList()!![0]
                 randomRecipeName.text = randomMeal.getStrMeal()
@@ -84,7 +81,7 @@ class MainActivity : AppCompatActivity() {
                     .load(randomMeal.getStrMealThumb())
                     .into(randomRecipeImage)
 
-            }, Response.ErrorListener {
+            }, {
                 fun onErrorResponse(error: VolleyError) {
                     randomRecipeName.text = "That didn't work!"
                 }
@@ -106,7 +103,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onAllMealsClick(view: View?) {
+        //createMealList()
+
         val intent = Intent(this, MealsRecyclerActivity::class.java)
+       // intent.putExtra("MEALS", arrayListOf(mealsAPI))
         startActivity(intent)
     }
 
@@ -116,79 +116,136 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onViewClick(view: View?) {
-        createRecipeList()
+        //createRecipeList()
         val intent = Intent(this, MealsRecyclerActivity::class.java)
         startActivity(intent)
     }
 
-    fun createMealList() {
-        mealsAPI.clear()
-        val alphabet = arrayOf(
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-        )
-        for (s in alphabet) {
-
-            val url = "https://www.themealdb.com/api/json/v1/1/search.php?f=$s" //string templates
-
-            val queue: RequestQueue = Volley.newRequestQueue(this@MainActivity)
-            val gson: Gson = GsonBuilder().setPrettyPrinting().create()
-
-            val stringRequest = StringRequest(
-                Request.Method.GET, url,
-
-                Response.Listener { response ->
-
-                    try {
-                        if (response != "{\"meals\":null}") {
-                            jObject = JSONObject(response)
-                            val jsonArray = jObject.getJSONArray("meals")
-                            for (j in 0 until jsonArray.length()) {
-                                val x = jsonArray.getJSONObject(j)
-                                val meal: MealAPI = gson.fromJson(x.toString(), MealAPI::class.java)
-                                mealsAPI.add(meal)
-                            }
-                        }
-                    } catch (e: JSONException) { //exception
-                        e.printStackTrace();
-                    }
-
-                },
-                Response.ErrorListener {
-                    fun onErrorResponse(error: VolleyError) {
-                    }
-                }
-            )
-            queue.add(stringRequest)
-        }
-    }
-
-    fun createRecipeList() {
-//        mealsUser.clear()
-//        val file = getExternalFilesDir(AddActivity.FOLDERNAME)
-//        val fileListing = file!!.listFiles()
-//        if (fileListing != null) {
-//            for (i in fileListing.indices) {
-//                val str = StringBuilder()
-//                try {
-//                    BufferedReader(FileReader(fileListing[i])).use { bufr ->
-//                        val fileName = fileListing[i].name
-//                        var text: String?
-//                        while (bufr.readLine().also { text = it } != null) {
-//                            str.append(text)
+//    fun createMealList() {
+//        //mealsAPI.clear()
+//
+//        val alphabet = arrayOf(
+//            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+//            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+//        )
+//        for (s in alphabet) {
+//
+//            val url = "https://www.themealdb.com/api/json/v1/1/search.php?f=$s" //string templates
+//
+//            val queue2: RequestQueue = Volley.newRequestQueue(this)
+//            val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+//
+//            val stringRequest = StringRequest(
+//                Request.Method.GET, url, { response ->
+//
+//                    if (response != "{\"meals\":null}") {
+//                        val strResp = response.toString()
+//                        val jObj: JSONObject = JSONObject(strResp)
+//                        val jArr: JSONArray = jObj.getJSONArray("meals")
+//
+//                        for (i in 0 until jArr.length()) {
+//                            val jInner: JSONObject = jArr.getJSONObject(i)
+//                            val newMeal: MealAPI =
+//                                gson.fromJson(jInner.toString(), MealAPI::class.java)
+//
+//                            mealsAPI.add(newMeal)
 //                        }
-//                        val recipe = MealUser(fileName, str.toString())
-//                        mealsUser.add(recipe)
 //                    }
-//                } catch (e: FileNotFoundException) {
-//                    e.printStackTrace()
-//                    Toast.makeText(this, "File not found.", Toast.LENGTH_LONG).show()
-//                } catch (e: IOException) {
-//                    e.printStackTrace()
-//                    Toast.makeText(this, "Something went wrong. Try again.", Toast.LENGTH_LONG)
-//                        .show()
-//                }
-//            }
+//
+//
+//                    //processResponse(response)
+//
+//
+////                    if (response != "{\"meals\":null}") {
+////                        val dataMeal: DataMeal = gson.fromJson(response, DataMeal::class.java)
+////                        myList = dataMeal.getMealsList()!!
+////                        mealsAPI.addAll(myList)
+//////                        for (i in dataMeal.getMealsList()!!) {
+//////                            mealsAPI.add(i)
+//////                        }
+////                    }
+//                }, { Toast.makeText(this, "That didn't work", Toast.LENGTH_LONG).show() })
+//
+//            queue2.add(stringRequest)
 //        }
-    }
+//    }
+//
+////    val dataMeal: DataMeal = gson.fromJson(response, DataMeal::class.java)
+////    randomMeal = dataMeal.getMealsList()!![0]
+////    randomRecipeName.text = randomMeal.getStrMeal()
+////    Picasso.get()
+////    .load(randomMeal.getStrMealThumb())
+////    .into(randomRecipeImage)
+//
+//    fun processResponse(response: String) {
+//        //var myList = mutableListOf<Meal>()
+//
+//        if (response != "{\"meals\":null}") {
+//            val dataMeal: DataMeal = Gson().fromJson(response, DataMeal::class.java)
+//            val myList = dataMeal.getMealsList()!!
+//            mealsAPI.toMutableList().addAll(myList)
+//        }
+//    }
+
+//    fun createRecipeList() {
+////        mealsUser.clear()
+////        val file = getExternalFilesDir(AddActivity.FOLDERNAME)
+////        val fileListing = file!!.listFiles()
+////        if (fileListing != null) {
+////            for (i in fileListing.indices) {
+////                val str = StringBuilder()
+////                try {
+////                    BufferedReader(FileReader(fileListing[i])).use { bufr ->
+////                        val fileName = fileListing[i].name
+////                        var text: String?
+////                        while (bufr.readLine().also { text = it } != null) {
+////                            str.append(text)
+////                        }
+////                        val recipe = MealUser(fileName, str.toString())
+////                        mealsUser.add(recipe)
+////                    }
+////                } catch (e: FileNotFoundException) {
+////                    e.printStackTrace()
+////                    Toast.makeText(this, "File not found.", Toast.LENGTH_LONG).show()
+////                } catch (e: IOException) {
+////                    e.printStackTrace()
+////                    Toast.makeText(this, "Something went wrong. Try again.", Toast.LENGTH_LONG)
+////                        .show()
+////                }
+////            }
+////        }
+//    }
 }
+
+
+//            val stringRequest: StringRequest = StringRequest(
+//                Request.Method.GET, url, Response.Listener { response ->
+////                    Toast.makeText(this, response.toString(), Toast.LENGTH_LONG).show()
+////
+////                    val listOfStrings = Gson().fromJson(response, mutableListOf<String>().javaClass)
+////
+////                    for (j in 0 until listOfStrings.size) {
+////                        val asObject = Gson().fromJson(listOfStrings[j], MealAPI::class.java)
+////                        mealsAPI.add(asObject)
+////                    }
+//
+//                    try {
+//                        if (response != "{\"meals\":null}") {
+//                            jObject = JSONObject(response)
+//                            val jsonArray = jObject.getJSONArray("meals")
+//                            for (j in 0 until jsonArray.length()) {
+//                                val x = jsonArray.getJSONObject(j)
+//                                val meal: MealAPI = gson.fromJson(x.toString(), MealAPI::class.java)
+//
+//                                mealsAPI.add(meal)
+//                            }
+//                        }
+//                    } catch (e: JSONException) { //exception
+//                        e.printStackTrace();
+//                    }
+//                }, Response.ErrorListener {
+////                    fun onErrorResponse(error: VolleyError) {
+//                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+//                    //}
+//                }
+//            )
--- app/src/main/java/com/example/bonappetitkotlin/MealsRecyclerActivity.kt
+++ app/src/main/java/com/example/bonappetitkotlin/MealsRecyclerActivity.kt
@@ -1,22 +1,139 @@
 package com.example.bonappetitkotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
-class MealsRecyclerActivity: AppCompatActivity(), OnItemClickListener  {
-    lateinit var mealList: ArrayList<Meal>
+open class MealsRecyclerActivity : AppCompatActivity(), OnItemClickListener {
+    open val mealList: MutableList<MealAPI> = mutableListOf()
+    open var dataMeal: DataMeal = DataMeal()
+    open var meals: ArrayList<MealAPI> = ArrayList<MealAPI>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_meals)
 
-        mealList = intent.getSerializableExtra("MEALS") as ArrayList<Meal>
-        
+        mealList.addAll(createMealList())
+
+        val mealListFinal = mealList as ArrayList<Meal>
+
+        val rvMeals = findViewById<RecyclerView>(R.id.rvAreasList)
+        rvMeals.setHasFixedSize(true)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        val adapter: RecyclerView.Adapter<*> = MealsAdapter(mealListFinal, this)
+
+        rvMeals.layoutManager = layoutManager
+        rvMeals.adapter = adapter
     }
 
     override fun onItemClick(view: View?, position: Int) {
-        TODO("Not yet implemented")
+        val intent = Intent(this, MealActivity::class.java)
+        intent.putExtra("MealExtra", mealList[position])
+        startActivity(intent)
+    }
+
+    fun createMealList(): MutableList<MealAPI> {
+
+        fun makeRequest(s: String) {
+
+            meals.clear()
+
+            val url =
+                "https://www.themealdb.com/api/json/v1/1/search.php?f=$s" //string templates
+
+            val queue: RequestQueue = Volley.newRequestQueue(this@MealsRecyclerActivity)
+            val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+
+            fun processRequest(response: String){
+                dataMeal = gson.fromJson(response, DataMeal::class.java)
+                meals = dataMeal.getMealsList()!!
+            }
+
+            val stringRequest: StringRequest =
+                StringRequest(Request.Method.GET, url, { response ->
+                    try {
+                        //processRequest(response)
+                        dataMeal = gson.fromJson(response, DataMeal::class.java)
+                        meals = dataMeal.getMealsList()!!
+                        Toast.makeText(this, meals[0].getStrMeal(), Toast.LENGTH_LONG).show()
+                        //dataMeal = gson.fromJson(response, DataMeal::class.java)
+                        //meals = dataMeal.getMealsList()!!
+                        //return@StringRequest
+                    } catch (e: KotlinNullPointerException) {
+                    }
+
+                }, { Toast.makeText(this, "That didn't work", Toast.LENGTH_LONG).show() }
+            )
+            //queue.cache.clear()
+            queue.add(stringRequest)
+        }
+
+        val myList: MutableList<MealAPI> = mutableListOf()
+
+        val alphabet = arrayOf(
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        )
+
+        for (s in alphabet) {
+            makeRequest(s)
+            myList.addAll(meals)
+        }
+
+        return myList
+
+
+//            val stringRequest = StringRequest(
+//                Request.Method.GET, url, { response ->
+//                    try {
+//                        dataMeal = gson.fromJson(response.toString(), DataMeal::class.java)
+//                        meals = dataMeal.getMealsList()!!
+//                        //Toast.makeText(this, meals[0].getStrMeal(), Toast.LENGTH_LONG).show()
+//
+////                        val strResp = response.toString()
+////                        val jObj = JSONObject(strResp)
+////                        val jArr: JSONArray = jObj.getJSONArray("meals")
+////
+////                        for (i in 0 until jArr.length()) {
+////                            val jInner: JSONObject = jArr.getJSONObject(i)
+////                            val newMeal: MealAPI =
+////                                gson.fromJson(jInner.toString(), MealAPI::class.java)
+////
+////                            mealList.add(newMeal)
+////                        }
+//                    } catch (e: KotlinNullPointerException) {
+//                    }
+//
+//                }, { Toast.makeText(this, "That didn't work", Toast.LENGTH_LONG).show() })
+//
+//            stringRequest.setShouldCache(false)
+//            queue.cache.clear()
+//            queue.add(stringRequest)
+//
+//            try {
+//                myList.addAll(meals)
+//                Toast.makeText(this, myList[0].getStrMeal(), Toast.LENGTH_LONG).show()
+//            } catch (e: RuntimeException) {
+//
+//            }
+//        }
+//        mealList.addAll(myList)
+//    }
+
     }
 }
